6ef3cd467ddb578717e821bce52771aadbff1357
Vlada650 <vladano46@gmail.com>
2021-08-12 12:22:26 +0300

    new

---
+++ firstapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PieChart from './components/PieChart'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <PieChart />
+    </div>
+  )
+}
+export default App
