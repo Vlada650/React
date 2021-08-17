7403013678c1143780ad1ffbf618ccc1b18e4457
Vlada650 <vladano46@gmail.com>
2021-08-17 13:28:49 +0300

    hero app week26task1

---
+++ heroapp/src/components/hero.jsx
@@ -0,0 +1,16 @@
+import './hero.css'
+
+function Hero(props) {
+    return (
+        <section className="hero-card">
+            <img src={props.url} alt="hero" />
+            <div className="hero-name">Имя: {props.name}</div>
+            <div className="hero-alterEgo">Прозвище: {props.alterEgo}</div>
+            <div className="hero-occupation">Род деятельности:{props.occupation}</div>
+            <div className="hero-superpower">Суперсилы: {props.superpower}</div>
+            <div className="hero-more">Больше: {props.more}</div>
+        </section>
+    );
+}
+
+export default Hero;
