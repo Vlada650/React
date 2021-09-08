1bc3570daa2f774c40f05448d1958c8eba482484
Vlada650 <vladano46@gmail.com>
2021-09-08 10:08:05 +0300

    edit style week28

--- tariffsapp/src/components/tariffs.jsx
+++ tariffsapp/src/components/tariffs.jsx
@@ -6,13 +6,18 @@ function List(props) {
     const [checked, setChecked] = useState(false)
 
     return (
-        <div className={"mainContainer" + (props.price == 550 ? " selected__card" : "")} {...props} onClick={() => { setChecked(true) }} id="container" tabIndex="0" onBlur={() => { setChecked(false) }}> {
+        <div className="mainContainer" {...props} onClick={() => { setChecked(true) }} id="container" tabIndex="0" onBlur={() => { setChecked(false) }}> {
             checked
                 ? (<div className="checked" >
-                    <button className="checked__circle">&#10004;</button>
+                    <div className={"mainContainer__header" + " selected__tariff + selected__header"}>{props.header}</div>
+                    <div className={"mainContainer__price" + " selected__tariff"}>
+                        <span className="mainContainer__currency">руб</span>  <span className="mainContainer__sum"> {props.price} </span> <span className="mainContainer__month">/мес</span>
+                    </div  >
+                    <div className="mainContainer__speed">{props.speed}</div>
+                    <div className="mainContainer__text">Объем включенного трафика не ограничен</div>
                 </div>)
-                : (<div> <div className={"mainContainer__header" + (props.price == 550 ? " selected__tariff + selected__header" : "")}>{props.header}</div>
-                    <div className={"mainContainer__price" + (props.price == 550 ? " selected__tariff" : "")}>
+                : (<div> <div className="mainContainer__header">{props.header}</div>
+                    <div className="mainContainer__price">
                         <span className="mainContainer__currency">руб</span>  <span className="mainContainer__sum"> {props.price} </span> <span className="mainContainer__month">/мес</span>
                     </div  >
                     <div className="mainContainer__speed">{props.speed}</div>
