05a449bdd62476d8c8c76ed929ca98953e4b68bc
Vlada650 <vladano46@gmail.com>
2021-10-11 12:06:03 +0300

    add example

---
+++ heroapp/src/components/Example-loginform.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import './hero.css'
+
+export default function Login() {
+
+    const [errorMes, setError] = useState(false)
+    const [value, setValue] = useState({
+        login: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setValue({ [e.target.name]: e.target.value })
+    }
+    useEffect(() => {
+        if (value.login || value.password === "") {
+            setError(true)
+        } else { setError(false) }
+    }, [value])
+
+    return (
+        <form>
+            <input type="text" value={value.login} onChange={handleChange} id="login" className="fadeIn" name="login" placeholder="login" />
+            <input type="password" value={value.password} id="password" onChange={handleChange} className="fadeIn" name="password" placeholder="password" />
+            <button className="btn" >Log In</button>
+            {errorMes ? (<span>Заполните все поля!</span>) : ''}
+        </form>
+    );
+}
--- heroapp/src/components/hero.jsx
+++ heroapp/src/components/hero.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import './hero.css'
 
 function Hero(props) {
