3556903143d738e27628fefe950064f4c33f1098
Vlada650 <vladano46@gmail.com>
2021-11-06 13:29:28 +0200

    commit

--- learnitapp/src/components/cardSlider.jsx
+++ learnitapp/src/components/cardSlider.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useRef } from "react";
-import {observer, inject} from "mobx-react";
+import React, { useEffect, useRef, useState } from "react";
 
-const CardSlider =  inject(['cardSliderStore'])(observer(({ cardSliderStore, words }) => {
+const CardSlider =  ({words }) => {
     
     const ref = useRef();
     useEffect(() => ref.current.focus(), []);
 
-    /*const [position, setPosition] = useState(0)
+    const [position, setPosition] = useState(0)
     const [pushed, setPushed] = useState(false);
     const [learned, setLearned] = useState(0)
     
@@ -28,23 +27,23 @@ const CardSlider =  inject(['cardSliderStore'])(observer(({ cardSliderStore, wor
     const btnTranslate = () => {
         setPushed(true);
         setLearned(learned + 1);
-    }*/
+    }
     return (
         < div className="slider" >
             <div className="slider-container">
-                <button className="slider-container__btn" onClick={cardSliderStore.prevCardHandler}>prev</button>
+                <button className="slider-container__btn" onClick={prevCardHandler}>prev</button>
                 <div className="card">
-                    <div className="card__word">{words[cardSliderStore.position].english}</div>
-                    <div className="card__scription">{words[cardSliderStore.position].transcription}</div>{
-                        cardSliderStore.pushed ? (<div className="card__translate">{words[cardSliderStore.position].russian}</div>)
-                            : (<div className="card__button"><button className="card__button-add" ref={ref} onClick={cardSliderStore.btnTranslate}>Показать перевод</button></div>)}
+                    <div className="card__word">{words[position].english}</div>
+                    <div className="card__scription">{words[position].transcription}</div>{
+                        pushed ? (<div className="card__translate">{words[position].russian}</div>)
+                            : (<div className="card__button"><button className="card__button-add" ref={ref} onClick={btnTranslate}>Показать перевод</button></div>)}
                 </div>
-                <button className="slider-container__btn" onClick={cardSliderStore.nextCardHandler}>next</button>
+                <button className="slider-container__btn" onClick={nextCardHandler}>next</button>
             </div>
-            <div className="slider-numbers">{cardSliderStore.position + 1} / {words.length}</div>
-            <div className="slider-numbers">Изучено слов: {cardSliderStore.learned}</div>
+            <div className="slider-numbers">{position + 1} / {words.length}</div>
+            <div className="slider-numbers">Изучено слов: {learned}</div>
         </div >
     )
-}))
+};
 
 export default CardSlider;
